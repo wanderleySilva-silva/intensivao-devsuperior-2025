32170e4c6dd60b96fffe48f04f67278b23c92f60
wanderleySilva-silva <jwps.17@hotmail.com>
2025-01-23 11:05:54 -0300

    modificado webconfig

--- src/main/java/com/devsuperior/dslist/config/WebConfig.java
+++ src/main/java/com/devsuperior/dslist/config/WebConfig.java
@@ -1,28 +1,27 @@
 package com.devsuperior.dslist.config;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.servers.Server;
 
 @Configuration
 public class WebConfig {
 	
-	@Value("${cors.origins}")
-	private String corsOrigins;
+	/*
+	 * @Value("${cors.origins}") private String corsOrigins;
+	 * 
+	 * @Bean public WebMvcConfigurer corsConfigurer() { return new
+	 * WebMvcConfigurer() {
+	 * 
+	 * @Override public void addCorsMappings(CorsRegistry registry) {
+	 * registry.addMapping("/**") .allowedOrigins(corsOrigins) .allowedMethods("*")
+	 * .allowedHeaders("*") .allowCredentials(true); } }; }
+	 */
 	
 	@Bean
-    public WebMvcConfigurer corsConfigurer() {
-        return new WebMvcConfigurer() {
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**")
-                        .allowedOrigins(corsOrigins)
-                        .allowedMethods("*")
-                        .allowedHeaders("*")
-                        .allowCredentials(true);
-            }
-        };
-    }
+	public OpenAPI customOpenAPI() {
+		return new OpenAPI().addServersItem(new Server().url("https://intensivao-devsuperior-2025-production.up.railway.app/"));
+	}
 }
